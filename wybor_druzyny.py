6c5b634a805d6fccd26a7ec5bd661bc374e5ef73
Alicja <ala.holowiecka@gmail.com>
2019-12-23 17:07:59 +0100

    Zamiast wyświetlania druzyny 6 pokemonów, wyswietla 3 mega ewolucje i 10 zwykłych pokemonów (wg takich samych kryteriów jak wcześniej), tak aby użytkownik mógł sam skonstruować swoją drużynę

--- wybor_druzyny.py
+++ wybor_druzyny.py
@@ -25,22 +25,30 @@ if(czy_lider == "t"):
     twoj_typ = input("podaj typ ")
     twoj_typ = twoj_typ.lower().capitalize()
     if (twoj_typ in typy):
-        twoj_mega = mega[mega.Type1 == twoj_typ].iloc[0].Name
-        twoje_niemega = list(niemega[niemega.Type1 == twoj_typ].iloc[0:5].Name)
+        twoje_mega = mega[mega.Type1 == twoj_typ].iloc[0:3]
+        twoje_niemega = niemega[niemega.Type1 == twoj_typ].iloc[0:10]
     else:
         raise Exception('nieprawidlowy typ. lista mozliwych typow to {}'.format(typy))
 elif(czy_lider == "n"):
-    twoj_mega = mega.iloc[0].Name
-    twoje_niemega = list(niemega.iloc[0:5].Name)
+    twoje_mega = mega.iloc[0:3]
+    twoje_niemega = niemega.iloc[0:10]
 else:
     raise Exception('nieprawidlowa odpowiedz na pytanie o lidera')
-#%%    
-druzyna = [twoj_mega]+ twoje_niemega
-#%%
-twoja_druzyna = pd.DataFrame()
+    
 
-for poke in druzyna:
-    twoja_druzyna = pd.concat([twoja_druzyna, df[df.Name==poke]])
 #%%
-print("\t\t\t TWOJA DRUŻYNA \n")
-print(twoja_druzyna.loc[:,['Name', 'Type1', 'Total']])
+print("\t\t Proponowane dla Ciebie mega ewolucje \n\n")
+print(twoje_mega.loc[:, ['Name', 'Type1', 'Type2', 'Total']])  
+#%%
+print('\t\t Proponowane dla Ciebie pokemony')
+print(twoje_niemega.loc[:, ['Name', 'Type1', 'Type2', 'Total']])  
+#%%    
+#druzyna = twoje_mega+ twoje_niemega
+#%%
+#twoja_druzyna = pd.DataFrame()
+
+#for poke in druzyna:
+ #   twoja_druzyna = pd.concat([twoja_druzyna, df[df.Name==poke]])
+#%%
+#print("\t\t\t TWOJA DRUŻYNA \n")
+#print(twoja_druzyna.loc[:,['Name', 'Type1', 'Total']])
